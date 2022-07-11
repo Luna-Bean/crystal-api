f1808386742318bee55a6038df75ab71c0582e76
lunarvibes45 <tealdeal@tutanota.com>
2022-07-11 00:02:37 -0700

    changed a lot of stuff

--- server.js
+++ server.js
@@ -1,10 +1,63 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
 
-server.use(middlewares);
-server.use(router);
+app.use(cors())
 
-server.listen(port);
+app.get('/', (req, res) => {
+    console.log('your response is working')
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+app.get('/api/:day', (req,res) =>{
+    const dayVariable = req.params.crystals.toLowerCase()
+    if(crystals[days]){
+        res.json(crystals[dayVariable])
+    }else{
+        res.json('please enter a day of the week!')
+    }
+})
+
+
+   const crystals = {
+        "monday": {
+            "stone": "Selenite", 
+            "planet": "Moon"
+        },
+        
+        "Tuesday" : {
+            "stone": "Jasper",
+            "planet": "Mars"
+            },
+    
+        "Wednesday": {
+            "stone":"Hematite",
+            "planet":"Mercury"
+        },
+
+        "Thursday": {
+            "stone":"Aventurine",
+            "planet": "Jupiter"
+                },
+
+        "Friday": {
+            "stone":"Rhodonite",
+            "planet": "Venus"
+                },
+
+        "Saturday":{
+            "stone": "Hematite",
+            "planet": "Saturn"
+                },
+
+        "Sunday": {
+            "stone": "Citrine",
+            "planet": "Sun"
+                }
+            }
+    
+    app.listen(process.env.PORT||PORT, () =>{
+        console.log('server be running')
+    })
